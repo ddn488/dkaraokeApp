60d755032ccaa2b2570edb898446462a95118d31
ddn488 <ddn488@gmail.com>
2015-09-24 20:33:26 -0700

    updated convert2mp4

--- dkaraokeApp/src/main/resources/batch/convert2Mp4.bat
+++ dkaraokeApp/src/main/resources/batch/convert2Mp4.bat
@@ -1,8 +1,7 @@
 @echo off &setlocal
 
-REM C:\Users\dennis>c:\ffmpeg\bin\ffmpeg.exe -i "e:\Karaoke\10007   Tinh Ta Voi Minh.Y Nhi.Unknown.vob" -b:a -acodec 
-libfaac -ab 128k -map 0:1 -map 0:2 -map 0:3 "d:\Karaoke\10007   TinhTa Voi Minh.Y Nhi.Unknown.mp4"
-REM   Or use mp3 codec: -c:a libmp3lame -b:a 128k -ar 44100
+REM C:\ffmpeg\bin\ffmpeg.exe -i "e:\Karaoke\10007   Tinh Ta Voi Minh.Y Nhi.Unknown.vob" -map 0:1 -map 0:2 -map 0:3 -c:a libmp3lame -b:a 128k -ar 44100 "d:\Karaoke\10007   TinhTa Voi Minh.YNhi.Unknown.mp4"
+REM Or use mp3 codec: -c:a libmp3lame -b:a 128k -ar 44100
 
 REM walk directory structure and convert each file in quiet mode
 set "sourcefolder=%~1"
@@ -19,13 +18,13 @@ for /R "%sourcefolder%" %%a in (*.vob) do (
     set "targetfolder=%targetfolder%!sourcepath:%sourcefolder%=!"
  
    if not exist !targetfolder!!targetfile! (
-        echo converting "%%~nxa" ... 
-        echo c:\ffmpeg\bin\ffmpeg -i "!sourcefile!" -map 0:1 -map 0:2 -map 0:3 "!targetfolder!!targetfile!"
-   	c:\ffmpeg\bin\ffmpeg -v quiet -i "!sourcefile!" -b:a -map 0:1 -map 0:2 -map 0:3 "!targetfolder!!targetfile!"
+        echo converting "%%~nxa" - file number: !nfile!
+rem        echo c:\ffmpeg\bin\ffmpeg -i "!sourcefile!" -c:a libmp3lame -b:a 128k -ar 44100 -map 0:1 -map 0:2 -map 0:3 "!targetfolder!!targetfile!"
+   	    c:\ffmpeg\bin\ffmpeg -v quiet -i "!sourcefile!" -c:a libmp3lame -b:a 128k -ar 44100 -map 0:1 -map 0:2 -map 0:3 "!targetfolder!!targetfile!"
     	set /a nfile+=1
     )
 
-    if  !nfile! equ 50 (
+    if  !nfile! equ 2 (
        goto :done	
     )    
 )
